4bb73de59f408e9cfb31943bca20a44d0ffa8df1
Sablinita <sablinahare@gmail.com>
2023-03-14 13:19:49 +0100

    solved merge

--- src/pages/Profile/ProfileStyle.tsx
+++ src/pages/Profile/ProfileStyle.tsx
@@ -4,5 +4,13 @@ import { GlobalStyles, theme } from '../../Global.style'
 export const InputDiv = styled.div`
 display: flex;
 flex-direction: column;
+margin-top:20px;
+gap: 14px;
+`
+
+export const Input = styled.input`
+width: 200px;
+height: 40px;
+border-radius: 8px;
 `
 
