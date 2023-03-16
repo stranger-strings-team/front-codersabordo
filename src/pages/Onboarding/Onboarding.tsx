f8427e8d9fc2c233c94f535b3cc63a86fcf70e78
Rosie <rosie.bradshaw3@gmail.com>
2023-03-16 16:07:11 +0100

    changes made

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -1,16 +1,29 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import QuestionButton from '../../components/QuestionButton/QuestionButton'
 import { BlueButton, PinkButton, PurpleButton, YellowButton } from '../../components/QuestionButton/questionButton.style'
 import { ParagraphContainer, AnswerImage, Container, DarkText, GlobalStyles, OrangeText } from '../../Global.style'
 import correct from '../../assets/correct.png'
+import {questionServices} from '../../services/questionServices'
 import { ThoughtBubbleStyled } from '../../components/ThoughtBubble/ThoughtBubble.style'
 
 
 type Props = {};
 
-const Onboarding = (props: Props) => {
+type QuestionsType = {_id: string, question:string, answer:[{text:string, isCorrect:boolean}], type:string, section:string, feedbackCorrect:string, feedbackIncorrect:string}
+
+function Onboarding (props: Props) {
 
   const [show, setShow] = useState(true);
+
+  const [questions, setQuestions] = useState<QuestionsType[]>([])
+  
+  const loadQuestions = async () => {
+      const questions = await questionServices;
+      setQuestions(questions);}
+    
+  useEffect(() => {
+  loadQuestions();
+  }, [])
 
   return (
     <Container>
