e376c23f51cac8ac2c3198cc295182d4a356efee
HelAige <helena.aige@gmail.com>
2023-03-22 11:12:48 +0100

    adminpages folder created

---
+++ src/AdminPages/CoderProgress.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import OrangeText from '../../Global.style'
+
+type Props = {}
+
+const CoderProgress = (props: Props) => {
+  return (
+    <div>
+        <OrangeText></OrangeText>
+    </div>
+  )
+}
+
+export default CoderProgress
---
+++ src/Tests/UserButton.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render } from '@testing-library/react'
+import { UserButton } from '../components/UserButton'
+
+test('renders content', () => {
+    // const userButton = {
+    //     content: 'This is a test',
+    //     important: true
+    // }
+
+    // const mockHandler = jest.fn()
+
+    // const component = render(<UserButton loggedInUser={null} />)
+    
+    // const button = component.getByText('This is a test')
+    // fireEvent.click(button)
+
+    // expect(mockHandler.mock.calls).toHaveLength(1)
+
+    //expect(component.container).toHaveTextContent('userButton.content')
+})
