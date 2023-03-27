4e9aacf219205c57a3509e020ed1fd6b6d2ce49c
Sablinita <sablinahare@gmail.com>
2023-03-27 14:11:29 +0200

    discharged changes in open-question

---
+++ src/services/userServices.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const apiBase = 'http://localhost:4000/api'
+
+const loginUrl = ("http://localhost:4000/api/auth/login")
+
+export const getUsersRequest = async () => {
+    return axios.get(apiBase);
+}
+
+export const postUserRequest = async (user: Object) => {
+    return axios.post(apiBase + "/user", user);
+}
+
+export const patchUserRequest = async (user: Object) => {
+    return axios.patch(apiBase + "/user", user);//Falta terminar esta funcionalidad
+}
+
+export const authUserRequest = async (auth: Object) => {
+    return axios.post(loginUrl, auth);
+}
