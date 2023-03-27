0c9dcc861848585a67f24e603c803c731c35c70c
HelAige <helena.aige@gmail.com>
2023-03-27 12:16:37 +0200

    implemented logout button

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -32,6 +32,11 @@ const Login = () => {
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPost({ ...post, [e.target.name]: e.target.value })
   };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('access_token');
+    navigate('/login');
+  };
 	
   return (
     <Container>
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Perfil from '../../assets/profile.png'
 import Spacer from '../../components/Spacer/Spacer'
 import { Container, OrangeText } from '../../Global.style'
-import { Myspan } from '../Login/LoginStyle'
+import { Button, Myspan } from '../Login/LoginStyle'
 import { Input, InputDiv} from './ProfileStyle'
 
 type Props = {}
 
-const Profile = (props: Props) => {
+const Profile = () => {
+
+    const navigate = useNavigate()
+
+    const handlerLogout = () => {
+      sessionStorage.removeItem("access_token");
+      navigate("/login")
+    }
+
   return (
     <Container>
         <h3><OrangeText>Tu perfil</OrangeText></h3>
@@ -21,6 +29,7 @@ const Profile = (props: Props) => {
         <Input type="text" placeholder='  Email'/>
         </InputDiv>
         <p><Link to="/create-password">Cambiar contraseña</Link></p>
+        <Button onClick={handlerLogout}>Logout</Button>
         
     </Container>
   )
