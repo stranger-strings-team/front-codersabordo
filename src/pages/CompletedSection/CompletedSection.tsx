14c920f0ec9183f58431d0d0068a58f8cd24682c
HelAige <helena.aige@gmail.com>
2023-03-28 11:05:01 +0200

    redirect button final section

--- src/pages/CompletedSection/CompletedSection.tsx
+++ src/pages/CompletedSection/CompletedSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 import Completed from '../../assets/completada.png'
 import { NextButton } from '../../components/NextButton'
 import Spacer from '../../components/Spacer/Spacer'
@@ -7,13 +8,20 @@ import { Container } from '../../Global.style'
 type Props = {}
 
 const CompletedSection = (props: Props) => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/roadmap')
+  }
+
   return (
     <Container>
         <h3>¡Muy bien!</h3>
         <img src={Completed} alt="happy rainbow"/>
         <p>¡Has acertado todas las preguntas de esta sección!</p>
         <Spacer size={45} axis='vertical' />
-        <NextButton/>
+        <NextButton onClick={handleClick}/>
     </Container>
   )
 }
