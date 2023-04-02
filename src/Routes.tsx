ab05ac20e4d7123e526d04c53367735b34e44867
mori <armigofire@gmail.com>
2023-04-02 11:41:10 +0200

    removed unused components

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/GamePages/Home/Home';
 import Layout from './components/Layout/Layout';
@@ -10,13 +10,11 @@ import Register from './pages/GamePages/RegisterPage/Register';
 import Onboarding from './pages/GamePages/Onboarding/Onboarding';
 import Roadmap from './pages/GamePages/Roadmap/Roadmap';
 import Profile from './pages/GamePages/Profile/Profile';
-import CreatePassword from './pages/GamePages/CreatePassword/CreatePassword';
 import ProfilesAdmin from './pages/AdminPages/ProfilesAdmin/ProfilesAdmin';
 import OpenQuestion from './pages/GamePages/OpenQuestion/OpenQuestion';
 import Final from './pages/GamePages/Final/Final';
 import QuizQuestions from './pages/AdminPages/quizQuestions/QuizQuestions';
-import PostQuestionForm from './pages/AdminPages/PatchQuestion/PatchQuestion';
-import PatchQuestion from './pages/AdminPages/PatchQuestion/PatchQuestion';
+import PostQuestionForm from './pages/AdminPages/PostQuestion/PostQuestionForm';
 import EliminateQuestion from './pages/AdminPages/EliminateQuestion/EliminateQuestion';
 import CoderProgress from './pages/AdminPages/CoderProgress/CoderProgress';
 import { NoMatch } from './pages/GamePages/NoMatch/NoMatch';
@@ -25,7 +23,6 @@ import { NoMatch } from './pages/GamePages/NoMatch/NoMatch';
 
 
 const CreateRoutes = () => (
-  
     <Routes>
       <Route index element={<Home/>}/>
       <Route path="/" element={<Layout />}>
@@ -35,7 +32,6 @@ const CreateRoutes = () => (
         <Route path="/roadmap" element={<Roadmap />}/>
         <Route path="/onboarding/:section" element={<Onboarding />}/>
         <Route path="/profile" element={<Profile/>}/>
-        <Route path="/create-password" element={<CreatePassword/>}/>
         <Route path="/open-question" element={<OpenQuestion/>}/>
         <Route path="/completed-section" element={<CompletedSection/>}/>
         <Route path="/final" element={<Final/>}/>
@@ -44,7 +40,6 @@ const CreateRoutes = () => (
         <Route path="/admin" element={<Admin/>}/>
         <Route path="/admin/question" element={<QuizQuestions />}/>
         <Route path="/admin/question/post" element={<PostQuestionForm />}/>
-        <Route path="/admin/question/edit" element={<PatchQuestion />} />
         <Route path="/admin/question/delete" element={<EliminateQuestion/>} />
         <Route path="/admin/admins" element={<ProfilesAdmin/>} />
         <Route path='/admin/coders' element={<CoderProgress />} />
